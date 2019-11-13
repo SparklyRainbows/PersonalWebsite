814d3fcf8404bd4cf93af51c5a08b3b86fd8014f
unknown <jasminezhang@live.com>
2019-11-13 10:35:07 -0800

    added scroll up for portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//#region Portfolio scroll
+
 function scrollTo(elementID) {
     document.getElementById(elementID).scrollIntoView();
 }
@@ -13,3 +15,44 @@ document.getElementById("characters-btn").onclick = function() {
 document.getElementById("traditional-btn").onclick = function() {
     scrollTo("traditional");
 }
+//#endregion
+
+//#region Show footer
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('footer').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    if (st > lastScrollTop && st > navbarHeight){
+        $('footer').removeClass('nav-down').addClass('nav-up');
+    } else {
+        if ((st + $(window).height() < $(document).height())) {
+            $('footer').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+document.getElementById("scrollup-icon").onclick = function() {
+    scrollTo("main-header");
+}
+
+//#endregion
