eaa528c3a811075abba8f5aa8d1b214e85f4ed8e
unknown <jasminezhang@live.com>
2019-11-25 15:05:31 -0800

    fixed carousel bug, added new images

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,62 +1,83 @@
 let carouselWidth = 1000; 
 
-let prevButton = document.getElementsByClassName("button-previous");
-let nextButton = document.getElementsByClassName("button-next");
-let imageRow = document.getElementsByClassName("carousel-image-row");
+let games = [
+    'quickdraw',
+    'osborneodyssey',
+    'arcticadventures'
+];
 
-var imageNums = Array(imageRow.length).fill(0);
+let prevButtonSuffix = "-prev";
+let nextButtonSuffix = "-next";
+let imageRowSuffix = "-image-row";
 
-function showNextImage(index) {
-    index--;
-    imageNums[index]++;
-	let offset = -carouselWidth * imageNums[index];
-    imageRow[index].style.left = offset + "px";
+var imageNums = {
+    'quickdraw': 0,
+    'osborneodyssey': 0,
+    'arcticadventures': 0,
+  };
 
-	checkControls(index);
+function showNextImage(game) {
+    imageNums[game]++;
+	let offset = -carouselWidth * imageNums[game];
+    document.getElementById(game + imageRowSuffix).style.left = offset + "px";
+
+	checkControls(game);
 }
 
-for (var i = 0; i < imageRow.length; i++) {
-    nextButton[i].onclick = function() {
-        console.log("next " + i);
-        showNextImage(i);
+document.getElementById(games[0] + nextButtonSuffix).onclick = function() {
+    showNextImage(games[0]);
+}
+
+document.getElementById(games[1] + nextButtonSuffix).onclick = function() {
+    showNextImage(games[1]);
+}
+
+document.getElementById(games[2] + nextButtonSuffix).onclick = function() {
+    showNextImage(games[2]);
+}
+
+document.getElementById(games[0] + prevButtonSuffix).onclick = function() {
+    showPrevImage(games[0]);
+}
+
+document.getElementById(games[1] + prevButtonSuffix).onclick = function() {
+    showPrevImage(games[1]);
+}
+
+document.getElementById(games[2] + prevButtonSuffix).onclick = function() {
+    showPrevImage(games[2]);
+}
+
+function showPrevImage(game) {
+    imageNums[game]--;
+	let offset = -carouselWidth * imageNums[game];
+    document.getElementById(game + imageRowSuffix).style.left = offset + "px";
+
+	checkControls(game);
+}
+
+var totalImages = {};
+
+for(var i = 0; i < games.length; i++) {
+    totalImages[games[i]] = document.getElementById(games[i] + imageRowSuffix).childElementCount - 1;
+    
+    if (!document.getElementById(games[i] + prevButtonSuffix).classList.contains("hidden")) {
+        document.getElementById(games[i] + prevButtonSuffix).classList.add("hidden");
     }
-
-    prevButton[i].onclick = function() {
-        console.log("prev " + i);
-        showPrevImage(i);
-    }
 }
 
-function showPrevImage(index) {
-    index--;
-    imageNums[index]--;
-	let offset = -carouselWidth * imageNums[index];
-    imageRow[index].style.left = offset + "px";
-
-	checkControls(index);
-}
-
-let totalImages = Array(imageRow.length);
-for (var i = 0; i < imageRow.length; i++) {
-    totalImages[i] = imageRow[i].childElementCount - 1;
-
-    if (!prevButton[i].classList.contains("hidden")) {
-        prevButton[i].classList.add("hidden");
-    }
-}
-
-function checkControls(index) {
-	if (imageNums[index] == 0) {
-		prevButton[index].classList.add("hidden");
+function checkControls(game) {
+	if (imageNums[game] == 0) {
+		document.getElementById(game + prevButtonSuffix).classList.add("hidden");
 	}
-	else if (prevButton[index].classList.contains("hidden")) {
-		prevButton[index].classList.remove("hidden");
+	else if (document.getElementById(game + prevButtonSuffix).classList.contains("hidden")) {
+		document.getElementById(game + prevButtonSuffix).classList.remove("hidden");
     }
     
-	if (imageNums[index] >= totalImages[index]) {
-		nextButton[index].classList.add("hidden");
+	if (imageNums[game] >= totalImages[game]) {
+		document.getElementById(game + nextButtonSuffix).classList.add("hidden");
 	}
-	else if (nextButton[index].classList.contains("hidden")) {
-		nextButton[index].classList.remove("hidden");
+	else if (document.getElementById(game + nextButtonSuffix).classList.contains("hidden")) {
+		document.getElementById(game + nextButtonSuffix).classList.remove("hidden");
 	}
 }
