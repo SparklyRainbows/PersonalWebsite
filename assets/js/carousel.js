e3ee37f378696d42b9abb6fab7999258d264f8cd
jazhang <jasminezhang@live.com>
2020-05-19 22:17:07 -0700

    added dating sim

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,10 +1,11 @@
-let carouselWidth = 1000; 
+let carouselWidth = 1000;
 
 let games = [
     'quickdraw',
     'osborneodyssey',
     'arcticadventures',
-    'switch'
+    'switch',
+    'datingsim'
 ];
 
 let prevButtonSuffix = "-prev";
@@ -15,7 +16,8 @@ var imageNums = {
     'quickdraw': 0,
     'osborneodyssey': 0,
     'arcticadventures': 0,
-    'switch': 0
+    'switch': 0,
+    'datingsim': 0
   };
 
 function showNextImage(game) {
@@ -42,6 +44,10 @@ document.getElementById(games[3] + nextButtonSuffix).onclick = function() {
     showNextImage(games[3]);
 }
 
+document.getElementById(games[4] + nextButtonSuffix).onclick = function() {
+    showNextImage(games[4]);
+}
+
 document.getElementById(games[0] + prevButtonSuffix).onclick = function() {
     showPrevImage(games[0]);
 }
@@ -58,6 +64,10 @@ document.getElementById(games[3] + prevButtonSuffix).onclick = function() {
     showPrevImage(games[3]);
 }
 
+document.getElementById(games[4] + prevButtonSuffix).onclick = function() {
+    showPrevImage(games[4]);
+}
+
 function showPrevImage(game) {
     imageNums[game]--;
 	let offset = -carouselWidth * imageNums[game];
@@ -70,7 +80,7 @@ var totalImages = {};
 
 for(var i = 0; i < games.length; i++) {
     totalImages[games[i]] = document.getElementById(games[i] + imageRowSuffix).childElementCount - 1;
-    
+
     if (!document.getElementById(games[i] + prevButtonSuffix).classList.contains("hidden")) {
         document.getElementById(games[i] + prevButtonSuffix).classList.add("hidden");
     }
@@ -83,7 +93,7 @@ function checkControls(game) {
 	else if (document.getElementById(game + prevButtonSuffix).classList.contains("hidden")) {
 		document.getElementById(game + prevButtonSuffix).classList.remove("hidden");
     }
-    
+
 	if (imageNums[game] >= totalImages[game]) {
 		document.getElementById(game + nextButtonSuffix).classList.add("hidden");
 	}
